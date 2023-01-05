6c0e3e98fc044b7cd10621d3949fd0e1890ab2e4
Mr-Randell <randell.onyango@student.moringaschool.com>
2023-01-06 00:57:18 +0300

    Feat: added on hover styling + component responsiveness

--- src/components/Inventories.jsx
+++ src/components/Inventories.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { BiDotsVerticalRounded } from "react-icons/bi"
 
 const Inventories = () => {
   return (
     <section id="inventories">
-        <div className="container flex flex-col h-screen justify-center">
+        <div className="container flex flex-col h-screen items-center px-6 mx-auto justify-center">
             {/* Top Items */}
-            <div className="container flex justify-center items-center pt-6 mx-auto mt-10 space-y-0 md:space-x-10 md:flex-row">
-                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:w-1/4">
-                    <h5 className="max-w-sm text-2xl font-bold text-center md:text-3xl md:text-left">
+            <div className="container flex flex-col justify-center pt-6 mx-auto mt-10 space-y-12 md:space-y-0 md:space-x-10 md:flex-row">
+                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:hover:border-blue-800 md:w-1/4">
+                    <h5 className="max-w-sm text-2xl font-bold text-center md:hover:text-blue-800 md:text-3xl md:text-left">
                         Assets
                     </h5>
+                    {/* <BiDotsVerticalRounded /> */}
                     <p className="mx-w-sm text-2xl text-center md:text-left">
                         204
                     </p>
@@ -21,8 +23,8 @@ const Inventories = () => {
                         View All
                     </button>
                 </Link>
-                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:w-1/4">
-                    <h5 className="max-w-sm text-2xl font-bold text-center md:text-3xl md:text-left">
+                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:hover:border-blue-800 md:w-1/4">
+                    <h5 className="max-w-sm text-2xl font-bold text-center md:hover:text-blue-800 md:text-3xl md:text-left">
                         Assigned Assets
                     </h5>
                     <p className="mx-w-sm text-2xl text-center md:text-left">
@@ -35,8 +37,8 @@ const Inventories = () => {
                         View All
                     </button>
                 </Link>
-                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:w-1/4">
-                    <h5 className="max-w-sm text-2xl font-bold text-center md:text-3xl md:text-left">
+                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:hover:border-blue-800 md:w-1/4">
+                    <h5 className="max-w-sm text-2xl font-bold text-center md:hover:text-blue-800 md:text-3xl md:text-left">
                         Unassigned Assets
                     </h5>
                     <p className="mx-w-sm text-2xl text-center md:text-left">
@@ -52,9 +54,9 @@ const Inventories = () => {
             </div>
 
             {/* Bottom Items */}
-            <div className="container flex justify-center items-center pt-6 mx-auto mt-10 space-y-0 md:space-x-10 md:flex-row">
-                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:w-1/4">
-                    <h5 className="max-w-sm text-2xl font-bold text-center md:text-3xl md:text-left">
+            <div className="hidden container flex-col justify-center pt-6 mx-auto mt-10 space-y-12 md:flex md:space-y-0 md:space-x-10 md:flex-row">
+                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:hover:border-blue-800 md:w-1/4">
+                    <h5 className="max-w-sm text-2xl font-bold text-center md:hover:text-blue-800 md:text-3xl md:text-left">
                         Locations
                     </h5>
                     <p className="mx-w-sm text-2xl text-center md:text-left">
@@ -67,8 +69,8 @@ const Inventories = () => {
                         View All
                     </button>
                 </Link>
-                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:w-1/4">
-                    <h5 className="max-w-sm text-2xl font-bold text-center md:text-3xl md:text-left">
+                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:hover:border-blue-800 md:w-1/4">
+                    <h5 className="max-w-sm text-2xl font-bold text-center md:hover:text-blue-800 md:text-3xl md:text-left">
                         Products
                     </h5>
                     <p className="mx-w-sm text-2xl text-center md:text-left">
@@ -81,8 +83,8 @@ const Inventories = () => {
                         View All
                     </button>
                 </Link>
-                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:w-1/4">
-                    <h5 className="max-w-sm text-2xl font-bold text-center md:text-3xl md:text-left">
+                <Link to="#" className="flex flex-col items-left p-6 space-y-6 border md:hover:border-blue-800 md:w-1/4">
+                    <h5 className="max-w-sm text-2xl font-bold text-center md:hover:text-blue-800 md:text-3xl md:text-left">
                         Employees
                     </h5>
                     <p className="mx-w-sm text-2xl text-center md:text-left">
