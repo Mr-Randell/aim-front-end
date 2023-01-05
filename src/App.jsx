29c07cfa8381b300172039073f36006d1cf5432b
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2023-01-05 23:31:23 +0530

    done with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 // import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+import NavBar from './components/Nav/NavBar';
 // import {  } from "./components";
 import { Overview } from "./pages";
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return <>
+    <NavBar/>
     <div className="container flex-col items-center h-screen mx-auto">
       <Routes>
         <Route path="/" element={ <Overview />} />
       </Routes>
     </div>
-  )
+  </>
+  
 }
 
 export default App
