e2d7a7b7f3db4fdc3214619030f83c68c9bfc10b
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2023-01-07 15:03:01 +0530

    fixing footer branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react'
 // import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
-// import {  } from "./components";
+// import Footer from './components/Footer/Footer';
+import {Footer  } from "./components";
 import { Overview } from "./pages";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="container flex-col items-center h-screen mx-auto">
-      <Routes>
-        <Route path="/" element={ <Overview />} />
-      </Routes>
-    </div>
-  )
+    <>
+      {/* <div className="container flex-col items-center h-screen mx-auto"> */}
+        <Routes>
+          <Route path="/" element={<Overview />} />
+        </Routes>
+      {/* </div> */}
+      <Footer />
+    </>
+  );
 }
 
 export default App
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Performance from "./Performance";
 import Features from "./Features";
 import Hero from "./Hero";
+import Footer from "./Footer/Footer";
 
 export {
     Performance,
     Features,
     Hero,
+    Footer,
 };
