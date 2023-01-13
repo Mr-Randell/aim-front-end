e28a0254a4ba6c66bf085a510f2b0f4a564df856
Mr-Randell <randell.onyango@student.moringaschool.com>
2023-01-13 20:36:06 +0300

    Feat: updated codebase for login, signup & dashboard

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-function Login() {
+const Login = () => {
   return (
     <div className="h-screen flex flex-col justify-center">
-        <div className="container flex flex-col-reverse justify-center items-center px-6 mx-auto md:pt-10 md:border-2 md:mt-1 space-y-0 md:space-y-0 md:flex-row">
+        <div className="container flex flex-col-reverse justify-center items-center px-6 space-y-0 md:mt-1 md:space-y-0 md:flex-row transition ease-in-out delay-150 hover-translate-y-1 hover:scale-110 hover:md:shadow-2xl duration-300 ...">
             <div className="flex flex-col items-center md:mb-32 md:space-y-12 md:relative md:w-1/2">
                 <form className="flex flex-col pl-4 w-full md:m-20 ">
                     <div className="flex mt-4">
                         <div className="mt-10 w-full md:mt-60">
                             <input
                             type="text"
-                            className="border-b-2 p-3 bg-transparent placeholder:text-2xl placeholder-current mb-3 w-full"
+                            className="border-b-2 p-3 bg-transparent placeholder:text-2xl placeholder-current mb-6 w-full"
                             placeholder="Username"
                             />
                             <input
                             type="Password"
-                            className="border-b-2 p-3 bg-transparent placeholder:text-2xl placeholder-current  mb-3 w-full"
+                            className="border-b-2 p-3 bg-transparent placeholder:text-2xl placeholder-current  mb-6 w-full"
                             placeholder="Password"
                             />
                             <button
-                            className="bg-green-700 text-white font-bold text-2xl py-4 w-full  mt-3 left-0 sm:bg-green-600 md:hover:bg-green-600"
+                            className="text-white font-bold text-2xl py-4 w-full mt-3 md:bg-green-700 md:hover:bg-green-600 sm:bg-green-600"
                             type="submit"
                             >
                             Login
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,74 +2,84 @@ import React from "react";
 
 const Signup = () => {
   return (
-    <div className="container mx-auto border-4  border-spacing-6 max-w-screen-sm max-w-screen-xl xl:h-4/5">
-      <form className="flex flex-col m-10 ">
-        <input
-          type="text"
-          className="border-0 border-b-2 p-4 placeholder:text-2xl placeholder-current font-serif m-3"
-          placeholder="Username"
-        />
-        <input
-          type="password"
-          className="border-0 border-b-2 p-4 placeholder:text-2xl placeholder-current font-serif m-3"
-          placeholder="Password"
-        />
-        <input
-          type="text"
-          className="border-0 border-b-2 p-4 placeholder:text-2xl placeholder-current font-serif m-3"
-          placeholder="Company Name"
-        />
-        <input
-          type="tel"
-          className="border-0 border-b-2 p-4 placeholder:text-2xl placeholder-current font-serif m-3"
-          placeholder="Phone Number"
-        />
-        <div className="flex mt-4">
-          <div className="w-1/2 pr-2">
-            <input
-              type="text"
-              className="border-0 border-b-2 p-3 placeholder:text-2xl placeholder-current font-serif m-3 w-4/5"
-              placeholder="Country"
-            />
+    <div className="h-screen flex flex-col justify-center">
+      {/* <div className="flex border flex-col mb-4 text-2xl">Sign Up here</div> */}
+      <div className="container mx-auto border-spacing-6 md:h-flex transition ease-in-out delay-150 hover-translate-y-1 hover:scale-110 hover:md:shadow-2xl duration-300 ...">
+        <form className="flex flex-col m-10 ">
+          <input
+            type="text"
+            className="border-0 border-b-2 p-4 placeholder:text-2xl placeholder-current mb-3 mx-2"
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            className="border-0 border-b-2 p-4 placeholder:text-2xl placeholder-current mb-3 mx-2"
+            placeholder="Password"
+          />
+          <input
+            type="text"
+            className="border-0 border-b-2 p-4 placeholder:text-2xl placeholder-current mb-3 mx-2"
+            placeholder="Company Name"
+          />
+          <input
+            type="tel"
+            className="border-0 border-b-2 p-4 placeholder:text-2xl placeholder-current mb-3 mx-2"
+            placeholder="Phone Number"
+          />
+          <div className="flex mt-4">
+            <div className="w-1/2 pr-2">
+              <input
+                type="text"
+                className="border-0 border-b-2 p-3 placeholder:text-2xl placeholder-current mb-3 mx-2 w-11/12"
+                placeholder="Country"
+              />
+            </div>
+            <div className="w-1/2 pl-2">
+              <input
+                type="text"
+                className="border-0 border-b-2 p-3 placeholder:text-2xl placeholder-current mb-3 mx-2 w-11/12"
+                placeholder="Language"
+              />
+            </div>
           </div>
-          <div className="w-1/2 pl-2">
-            <input
-              type="text"
-              className="border-0 border-b-2 p-3 placeholder:text-2xl placeholder-current font-serif m-3 w-4/5"
-              placeholder="Language"
-            />
+          <div className="flex mt-4">
+            <div className="w-1/2 pr-2">
+              <input
+                type="text"
+                className="border-0 border-b-2 p-3 placeholder:text-2xl placeholder-current mb-3 mx-2 w-11/12"
+                placeholder="Company Size"
+              />
+            </div>
+            <div className="hidden md:block w-1/2 pl-2">
+              <input
+                type="text"
+                className="border-0 border-b-2 p-3 placeholder:text-2xl placeholder-current mb-3 mx-2 w-11/12"
+                placeholder="Primary Interest"
+              />
+            </div>
+            <div className="md:hidden sm:block w-1/2 pl-2">
+              <input
+                type="text"
+                className="border-0 border-b-2 p-3 placeholder:text-2xl placeholder-current mb-3 mx-2 w-11/12"
+                placeholder="Interests"
+              />
+            </div>
           </div>
-        </div>
-        <div className="flex mt-4">
-          <div className="w-1/2 pr-2">
-            <input
-              type="text"
-              className="border-0 border-b-2 p-3 placeholder:text-2xl placeholder-current font-serif m-3 w-4/5"
-              placeholder="Company Size"
-            />
+          <div className="container mx-auto border-2 m-5 p-3 border-spacing-4 bg-sky-500/5">
+            <p className="text-xl text-center font-serif">
+              {" "}
+              By clicking on Start Now, you accept our{" "}
+              <u>Subscription Agreement</u> and <u>Privacy Policy</u>
+            </p>
           </div>
-          <div className="w-1/2 pl-2">
-            <input
-              type="text"
-              className="border-0 border-b-2 p-3 placeholder:text-2xl placeholder-current font-serif m-3 w-4/5"
-              placeholder="Primary Interest"
-            />
-          </div>
-        </div>
-        <div className="container mx-auto border-2 m-5 p-3 border-spacing-4 bg-sky-500/5">
-          <p className="text-xl text-center font-serif">
-            {" "}
-            By clicking on Start Now, you accept our{" "}
-            <u>Subscription Agreement</u> and <u>Privacy Policy</u>
-          </p>
-        </div>
-        <button
-          className="float-right bg-blue-800 hover:bg-blue-700 text-white font-bold text-2xl py-4 sm:w-1/4 xl:w-1/6  right-0 font-serif"
-          type="submit"
-        >
-          Sign Up
-        </button>
-      </form>
+          <button
+            className="p-3 px-6 pt-2 text-white md:bg-blue-800 md:w-1/6 md:hover:bg-blue-700 sm:bg-blue-700"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
